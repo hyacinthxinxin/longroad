b2434edeb7e8fc74cee38b6184c0a16c15eda60d
hyacinthxinxin <fanxin0202@163.com>
2017-01-19 15:29:23 +0800

    TT

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,5 @@
 class Building < ApplicationRecord
+      belongs_to :user
       has_many :floors, dependent: :destroy
       validates :name, presence: true, length: { minimum: 2 }
 end
--- db/migrate/20170118075835_create_buildings.rb
+++ db/migrate/20170118075835_create_buildings.rb
@@ -5,8 +5,10 @@ class CreateBuildings < ActiveRecord::Migration[5.0]
       t.string :image_name
       t.string :socket_address
       t.integer :socket_port
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
+
   end
 end
