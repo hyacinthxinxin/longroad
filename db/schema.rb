2b36bb54e6aa324d020e45ac1e38b0d729dc895d
hyacinthxinxin <fanxin0202@163.com>
2017-01-19 13:03:34 +0800

    Use SSL and the Puma webserver in production

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118080821) do
+ActiveRecord::Schema.define(version: 20170119045522) do
 
   create_table "buildings", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20170118080821) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["building_id"], name: "index_floors_on_building_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
