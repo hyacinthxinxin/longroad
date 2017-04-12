40be1e32fc9fc8f9600e568a3507a8fcaa35e1be
hyacinthxinxin <fanxin0202@163.com>
2017-04-12 08:25:09 +0800

    x

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,26 @@
+class ApiController < ApplicationController
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def buildings
+    if user = User.find_by(id: params[:user_id])
+      render json: user.buildings.all
+    else
+      render json: {
+        result => false
+      }
+    end
+  end
+
+  def building
+    render json: @building = Building.find_by(id: params[:building_id])
+  end
+
+  def project
+    @building = Building.find_by(id: params[:building_id])
+                        .as_json(include:
+                                  { floors: { include:
+                                  { areas: { include:
+                                  { devices: { include: :cams } } } } } })
+    render json: @building
+  end
+end
